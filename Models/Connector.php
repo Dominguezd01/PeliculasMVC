756861d678ba94aa5e4de09fa3aca51501efcf0f
Dominguez <migueldominguezroman01@gmail.com>
2023-01-23 09:03:19 +0100

    23/01/2023

--- Models/Connector.php
+++ Models/Connector.php
@@ -1,19 +1,27 @@
 <?php
-class Connector
+class Connector extends PDO
 {
     private $conexion;
     function __construct(){
         try {
             //$dbRoute = ;
-            $this->conexion = new PDO('mysql:dbname=peliculas;host=127.0.0.1:3306', "root", "");
-            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            parent::__construct('mysql:dbname=peliculas;host=127.0.0.1;port=3306', "root", "");
+            /*
+            var_dump($this->conexion);
+            $this->conexion->set(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conexion->exec("SET CHARACTER SET UTF8");
-            
+            */
         } catch (Exception $e) {
             die($e->getMessage());
             echo "Linea del error" . $e->getLine();
         }
         
     }
+
+
+
+    
 }
+
+
 ?>
--- Models/peliculas.php
+++ Models/peliculas.php
@@ -1,15 +1,13 @@
 
 <?php
-    
     function peliculas_dramaticas($conexion){
-       
+        
         var_dump($conexion);
 
         $results = $conexion->query("Select titulo from peliculas");
 
         var_dump($results->fetchAll(0));
-    
-    }
 
+    }
 
 ?>
